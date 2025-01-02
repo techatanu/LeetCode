e468f90b6dbaad70c69f603820c9c77411b3c25c
Atanu <167212050+techatanu@users.noreply.github.com>
2025-01-02 11:12:46 +0530

    Add files via upload
    LeetCode Problem No #43 : Multiply_String

---
+++ Multiply_Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num3 = int(num1)
+        num4 = int(num2)
+        res = (num3 * num4)
+        return str(res)
